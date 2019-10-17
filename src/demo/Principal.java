db85f03873cc103345f6065cf62034fe5dd91886
DEveloper B <developerB@example.com>
2019-10-16 20:03:31 -0500

    Commit Before Merge to Local

--- src/demo/Principal.java
+++ src/demo/Principal.java
@@ -5,6 +5,9 @@ public class Principal {
 	public static void main(String[] args) {
 		//Master demo
 		System.out.println("Versión base en master");
+		
+		FeatureB fa = new FeatureB();
+		System.out.println(fa.getInfo());
 	}
 
 }
